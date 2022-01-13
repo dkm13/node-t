a4c506ed5fc603a096cae9d0f176d45f617bb3cc
dkm13 <dibrani_2@outlook.com>
2022-01-13 11:43:57 +0100

    first-commit

---
+++ 12-http.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("welcome to our home page");
+  }
+  if (req.url === "/about") {
+    res.end("here is our short history");
+  }
+  res.end(`<h1>Ooops!</h1>
+   <p>We cant find the page.</p>
+   <a href="/">Back home</a>
+  `);
+});
+
+server.listen(5000);
---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-export");
+require("./7-mind-grenade");
+
+sayHi("Dibran");
+sayHi(names.john);
+sayHi(names.peter);
